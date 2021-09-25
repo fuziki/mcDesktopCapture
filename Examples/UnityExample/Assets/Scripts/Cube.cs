647bdafd2cac2d060e49bf25ac0550ac45702bf1
fuziki <hymne.shannon.projects@gmail.com>
2021-09-25 14:22:57 +0900

    add display dropdown

--- Examples/UnityExample/Assets/Scripts/Cube.cs
+++ Examples/UnityExample/Assets/Scripts/Cube.cs
@@ -1,19 +1,34 @@
 using UnityEngine;
+using UnityEngine.UI;
 using mcDesktopCapture;
+using System.Linq;
+using System.Threading.Tasks;
 
 public class Cube : MonoBehaviour
 {
+    public Dropdown dropdown;
+
+    private DisplayProperty[] list = { };
+
+    private bool isRunning = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        var list = DesktopCapture.DisplayList;
+        list = DesktopCapture.DisplayList;
+        dropdown.options = list.ToList()
+            .ConvertAll(display => new Dropdown.OptionData($"{display.name}({display.id})"));
+
         DesktopCapture.StartCapture(list[0].id);
+        isRunning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(-0.2f, -0.3f, 0.4f);
+
+        if (!isRunning) return;
 
         var texture = DesktopCapture.GetCurrentFrame();
         if (texture == null) return;
@@ -27,4 +42,25 @@ public class Cube : MonoBehaviour
         Debug.Log("OnDisable");
         DesktopCapture.StopCapture();
     }
+
+    public void DropdownValueChanged()
+    {
+        Debug.Log($"dropdown: {dropdown.value}");
+        dropdown.enabled = false;
+        isRunning = false;
+        DesktopCapture.StopCapture();
+        Debug.Log("restarting...");
+        Restart();
+    }
+
+    async void Restart()
+    {
+        await Task.Delay(500);
+        DesktopCapture.StartCapture(list[dropdown.value].id);
+        await Task.Delay(500);
+        Debug.Log("restart!");
+        isRunning = true;
+        dropdown.enabled = true;
+    }
 }
+
